3e4c5e56faa8fd374a2df249baaf085f9b237331
yd-tw <yd960528@gmail.com>
2023-12-25 23:24:19 +0800

    add limelight get y and a data

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,15 +8,18 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Robot extends TimedRobot {
 
   private NetworkTable limelight = NetworkTableInstance.getDefault().getTable("limelight-front");
+  private double tx;
+  private double ty;
+  private double ta;
 
-/**
+  /**
     * 此函數在機器人首次啟動時運行，適用於任何
     * 初始化程式碼。
     */
   @Override
   public void robotInit() {}
 
-/**
+  /**
     * 無論模式如何，函數每 20 ms 呼叫一次。 將其用於診斷等項目
     * 您希望在停用、自主、遙控和測試期間運作。
     *
@@ -26,7 +29,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {}
 
-/**
+  /**
     * 這個自治（以及上面的選擇器代碼）顯示瞭如何在不同的之間進行選擇
     * 使用儀表板的自主模式。 可傳送的選擇器程式碼與 Java 一起使用
     * 智慧儀表板。 如果您喜歡 LabVIEW 儀表板，請刪除所有選擇器程式碼並
@@ -39,42 +42,47 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {}
 
-/** 該函數在自治期間被定期呼叫。 */
+  /** 該函數在自治期間被定期呼叫。 */
   @Override
   public void autonomousPeriodic() {}
 
-/** 當 teleop 啟用時，函數被呼叫一次。 */
+  /** 當 teleop 啟用時，函數被呼叫一次。 */
   @Override
   public void teleopInit() {}
 
-/** 此函數在操作員控制期間定期呼叫。 */
+  /** 此函數在操作員控制期間定期呼叫。 */
   @Override
   public void teleopPeriodic() {
-    double tx = limelight.getEntry("tx").getDouble(0.0);
+    tx = limelight.getEntry("tx").getDouble(0.0);
+    ty = limelight.getEntry("ty").getDouble(0.0);
+    ta = limelight.getEntry("ta").getDouble(0.0);
+
     SmartDashboard.putNumber("tx", tx);
+    SmartDashboard.putNumber("ty", ty);
+    SmartDashboard.putNumber("ta", ta);
   }
 
-/** 當機器人被停用時，函數被呼叫一次。 */
+  /** 當機器人被停用時，函數被呼叫一次。 */
   @Override
   public void disabledInit() {}
 
-/** 該函數在停用時會定期呼叫。 */
+  /** 該函數在停用時會定期呼叫。 */
   @Override
   public void disabledPeriodic() {}
 
-/** 啟用測試模式時，此函數被呼叫一次。 */
+  /** 啟用測試模式時，此函數被呼叫一次。 */
   @Override
   public void testInit() {}
 
-/** 在測試模式期間定期呼叫此函數。 */
+  /** 在測試模式期間定期呼叫此函數。 */
   @Override
   public void testPeriodic() {}
 
-/** 此函數在機器人首次啟動時呼叫一次。 */
+  /** 此函數在機器人首次啟動時呼叫一次。 */
   @Override
   public void simulationInit() {}
 
-/** 在模擬過程中定期呼叫此函數。 */
+  /** 在模擬過程中定期呼叫此函數。 */
   @Override
   public void simulationPeriodic() {}
 }
