f57ec7272f9e885c44e92bea8235643f63c759a1
yd-tw <yd960528@gmail.com>
2023-12-25 23:10:07 +0800

    Add annotation

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,72 +9,72 @@ public class Robot extends TimedRobot {
 
   private NetworkTable limelight = NetworkTableInstance.getDefault().getTable("limelight-front");
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+/**
+    * 此函數在機器人首次啟動時運行，適用於任何
+    * 初始化程式碼。
+    */
   @Override
   public void robotInit() {}
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
+/**
+    * 無論模式如何，函數每 20 ms 呼叫一次。 將其用於診斷等項目
+    * 您希望在停用、自主、遙控和測試期間運作。
+    *
+    * <p>它在模式特定週期函數之後運行，但在 LiveWindow 和
+    * SmartDashboard整合更新。
+    */
   @Override
   public void robotPeriodic() {}
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
+/**
+    * 這個自治（以及上面的選擇器代碼）顯示瞭如何在不同的之間進行選擇
+    * 使用儀表板的自主模式。 可傳送的選擇器程式碼與 Java 一起使用
+    * 智慧儀表板。 如果您喜歡 LabVIEW 儀表板，請刪除所有選擇器程式碼並
+    * 取消註解 getString 行以從陀螺儀下方的文字方塊中取得自動名稱
+    *
+    * <p>您可以透過在開關結構上添加額外的比較來添加額外的自動模式
+    * 下面帶有附加字串。 如果使用 SendableChooser 請確保將它們添加到
+    * 上面還有選擇器代碼。
+    */
   @Override
   public void autonomousInit() {}
 
-  /** This function is called periodically during autonomous. */
+/** 該函數在自治期間被定期呼叫。 */
   @Override
   public void autonomousPeriodic() {}
 
-  /** This function is called once when teleop is enabled. */
+/** 當 teleop 啟用時，函數被呼叫一次。 */
   @Override
   public void teleopInit() {}
 
-  /** This function is called periodically during operator control. */
+/** 此函數在操作員控制期間定期呼叫。 */
   @Override
   public void teleopPeriodic() {
     double tx = limelight.getEntry("tx").getDouble(0.0);
     SmartDashboard.putNumber("tx", tx);
   }
 
-  /** This function is called once when the robot is disabled. */
+/** 當機器人被停用時，函數被呼叫一次。 */
   @Override
   public void disabledInit() {}
 
-  /** This function is called periodically when disabled. */
+/** 該函數在停用時會定期呼叫。 */
   @Override
   public void disabledPeriodic() {}
 
-  /** This function is called once when test mode is enabled. */
+/** 啟用測試模式時，此函數被呼叫一次。 */
   @Override
   public void testInit() {}
 
-  /** This function is called periodically during test mode. */
+/** 在測試模式期間定期呼叫此函數。 */
   @Override
   public void testPeriodic() {}
 
-  /** This function is called once when the robot is first started up. */
+/** 此函數在機器人首次啟動時呼叫一次。 */
   @Override
   public void simulationInit() {}
 
-  /** This function is called periodically whilst in simulation. */
+/** 在模擬過程中定期呼叫此函數。 */
   @Override
   public void simulationPeriodic() {}
 }
